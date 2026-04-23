4e86a78015b48be5c6a2388168078c4fa07cb216
JashawnRogers <rogersjashawn20@gmail.com>
2026-04-22 23:38:07 -0700

    First commit

---
+++ src/main/java/com/jashawncodes/billtrackr/BilltrackrApplication.java
@@ -0,0 +1,13 @@
+package com.jashawncodes.billtrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilltrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilltrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jashawncodes/billtrackr/BilltrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jashawncodes.billtrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilltrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
