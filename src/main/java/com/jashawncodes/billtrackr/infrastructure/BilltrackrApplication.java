36a273dac472c32ba235274356a916d1d0ad5837
JashawnRogers <rogersjashawn20@gmail.com>
2026-04-24 11:43:58 -0700

    Initial package structure

--- src/main/java/com/jashawncodes/billtrackr/infrastructure/BilltrackrApplication.java
+++ src/main/java/com/jashawncodes/billtrackr/infrastructure/BilltrackrApplication.java
@@ -1,4 +1,4 @@
-package com.jashawncodes.billtrackr;
+package com.jashawncodes.billtrackr.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
